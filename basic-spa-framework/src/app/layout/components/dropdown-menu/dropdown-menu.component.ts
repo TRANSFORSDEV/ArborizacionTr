5789725fc5d70dd5b64ac7a1e6c75694d5445d25
Cquintero <carlosquinteronino@gmail.com>
2025-08-20 11:26:14 -0500

    Ajuste del modal

--- basic-spa-framework/src/app/layout/components/dropdown-menu/dropdown-menu.component.ts
+++ basic-spa-framework/src/app/layout/components/dropdown-menu/dropdown-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-dropdown-menu',
@@ -11,8 +11,14 @@ export class DropdownMenuComponent {
   @Input() isActive: boolean = false;
 
   isDropdownOpen = false;
+  @Output() closed = new EventEmitter<void>();
 
   toggleDropdown() {
     this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  closeDropdown() {
+    this.isDropdownOpen = false;
+    this.closed.emit();
   }
 }
--- basic-spa-framework/src/app/modules/censoarboreo/components/buscar-arbol/buscar-arbol.component.ts
+++ basic-spa-framework/src/app/modules/censoarboreo/components/buscar-arbol/buscar-arbol.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { CensoArboreoDto } from 'src/app/core/models/censoarboreo-dto.model';
 import { RequestStatus } from 'src/app/core/models/request-status.model';
@@ -11,64 +11,64 @@ import { CensoArboreoService } from 'src/app/core/services/censoarboreo.service'
 })
 export class BuscarArbolComponent {
   showModal = false;
-  dataList: CensoArboreoDto[] =[];
+  dataList: CensoArboreoDto[] = [];
   seleccionado: CensoArboreoDto = null;
-  form:FormGroup;
-  pageSize:number = 5;
-  page:number= 1;
-  collectionSize: number =1
+  form: FormGroup;
+  pageSize: number = 5;
+  page: number = 1;
+  collectionSize: number = 1
   statusDetail: RequestStatus = 'init';
-  isActividadPlantulaBusqueda : boolean = false;
-  @Output() selected= new EventEmitter();
-
+  isActividadPlantulaBusqueda: boolean = false;
+  @Output() selected = new EventEmitter();
+  @ViewChild('inputBuscar') inputBuscar!: ElementRef;
+  modalStyles: any = {};
   @Input()
-  set isActividadPlantula (param : boolean) {
-    
-    this.isActividadPlantulaBusqueda = param;      
+  set isActividadPlantula(param: boolean) {
+
+    this.isActividadPlantulaBusqueda = param;
   };
 
-constructor(
-  private formbuilder:FormBuilder,
-  private service  : CensoArboreoService){
+  constructor(
+    private formbuilder: FormBuilder,
+    private service: CensoArboreoService) {
 
     this.BuildForm();
     this.getCenso();
-}
-
-private getCenso(){
-  this.statusDetail = 'loading';
-  debugger;
-  if(this.isActividadPlantulaBusqueda) {
-    this.service.getFilterPlantula(this.field("filter").get().value, this.page ,this.pageSize ).subscribe(
-      respuesta => {
-        this.dataList = respuesta.data
-        this.collectionSize = respuesta.meta.totalCount;
-        this.pageSize =this.pageSize;
-        this.statusDetail = 'init';
-      }
-    );
-  } else {
-    this.service.get(this.field("filter").get().value, this.page ,this.pageSize ).subscribe(
-      respuesta => {
-        this.dataList = respuesta.data
-        this.collectionSize = respuesta.meta.totalCount;
-        this.pageSize =this.pageSize;
-        this.statusDetail = 'init';
-      }
-    );
   }
-  
-}
 
-  private BuildForm(){
+  private getCenso() {
+    this.statusDetail = 'loading';
+    debugger;
+    if (this.isActividadPlantulaBusqueda) {
+      this.service.getFilterPlantula(this.field("filter").get().value, this.page, this.pageSize).subscribe(
+        respuesta => {
+          this.dataList = respuesta.data
+          this.collectionSize = respuesta.meta.totalCount;
+          this.pageSize = this.pageSize;
+          this.statusDetail = 'init';
+        }
+      );
+    } else {
+      this.service.get(this.field("filter").get().value, this.page, this.pageSize).subscribe(
+        respuesta => {
+          this.dataList = respuesta.data
+          this.collectionSize = respuesta.meta.totalCount;
+          this.pageSize = this.pageSize;
+          this.statusDetail = 'init';
+        }
+      );
+    }
+
+  }
+
+  private BuildForm() {
     this.form = this.formbuilder.group({
-      filter:['', []],
-      nombreComun:['', []]
+      filter: ['', []],
+      nombreComun: ['', []]
     })
   }
 
-  Seleccionar(afiliado:CensoArboreoDto)
-  {
+  Seleccionar(afiliado: CensoArboreoDto) {
     this.seleccionado = afiliado;
     this.form.patchValue(afiliado);
     this.selected.emit(this.seleccionado);
@@ -80,7 +80,7 @@ private getCenso(){
     this.page = event.first / event.rows + 1
     this.pageSize = event.rows;
 
-    if(this.isActividadPlantulaBusqueda) {
+    if (this.isActividadPlantulaBusqueda) {
       this.service.getFilterPlantula(this.field("filter").get().value, this.page, this.pageSize).subscribe(
         response => {
           this.dataList = response.data
@@ -102,14 +102,14 @@ private getCenso(){
 
   }
 
-  Buscar(){
-    if(this.field("filter").get().value =="")
+  Buscar() {
+    if (this.field("filter").get().value == "")
       this.getCenso();
 
     this.statusDetail = 'loading';
-    debugger; 
-    if(this.isActividadPlantulaBusqueda) {
-      this.service.getFilterPlantula(this.field("filter").get().value, this.page ,this.pageSize ).subscribe(
+    debugger;
+    if (this.isActividadPlantulaBusqueda) {
+      this.service.getFilterPlantula(this.field("filter").get().value, this.page, this.pageSize).subscribe(
         afiliados => {
           this.dataList = afiliados.data
           this.collectionSize = afiliados.meta.totalCount;
@@ -118,7 +118,7 @@ private getCenso(){
         }
       );
     } else {
-      this.service.get(this.field("filter").get().value, this.page ,this.pageSize ).subscribe(
+      this.service.get(this.field("filter").get().value, this.page, this.pageSize).subscribe(
         afiliados => {
           this.dataList = afiliados.data
           this.collectionSize = afiliados.meta.totalCount;
@@ -127,18 +127,18 @@ private getCenso(){
         }
       );
     }
-    
+
   }
 
-  toggleModal(){
+  toggleModal() {
     this.showModal = !this.showModal;
   }
 
   field(name: string) {
     return {
-        get: () => this.form.get(name),
-        isValid: () => this.form.get(name).touched && this.form.get(name).valid
+      get: () => this.form.get(name),
+      isValid: () => this.form.get(name).touched && this.form.get(name).valid
     };
-}
+  }
 
 }
